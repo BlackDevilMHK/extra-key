b252c0837ce57bb35dc6e32c3685cf08940c89e0
B2A6K D3V1L-MHK <86191779+BlackDevilMHK@users.noreply.github.com>
2021-07-23 17:00:31 +0600

    Delete extra-key.sh

--- extra-key.sh
+++
@@ -1,1 +0,0 @@
-mkdir $HOME/.termux/ ;echo "extra-keys = [['ESC','/','-','HOME','UP','END'],['TAB','CTRL','ALT','LEFT','DOWN','RIGHT']]" >> $HOME/.termux/termux.properties && termux-reload-settings && sleep 1 && logout
